22e82478a6f4acf59c609189e2c49a30c0a13396
Francesco Sbaraglia <23255586+fsbaraglia@users.noreply.github.com>
2019-05-28 18:46:09 +0200

    last change for utf8

--- Crypto/aes128-gcm/src/aes128-gcm_decrypt.py
+++ Crypto/aes128-gcm/src/aes128-gcm_decrypt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-aes128-gcm_decrypt.py
+htb_aes128-gcm_decrypt.py
 Copyright (C) 2019 fsbaraglia
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -15,7 +15,7 @@ from Cryptodome.Cipher import AES
 from Cryptodome.Protocol.KDF import PBKDF2
 from optparse import OptionParser
 from termcolor import colored
-import base64, binascii
+import base64, binascii, hashlib
 
 ### Constants
 ALGORITHM_KEY_SIZE = 32
@@ -32,17 +32,20 @@ def decryptStr(base64Ciphertext, password):
     # Extract Variables for Decryption
     b_salt = encrypted[:AES.block_size]
     b_nonce = encrypted[AES.block_size:ALGORITHM_KEY_SIZE]
-    b_associatedText = encrypted[:ALGORITHM_KEY_SIZE]
-    b_ciphertext = encrypted[ALGORITHM_KEY_SIZE:]
+    b_associatedText = encrypted[:AES.block_size]
+    b_tag = encrypted[-16:]
+    b_ciphertext = encrypted[ALGORITHM_KEY_SIZE:-AES.block_size]
+
 
     # Derive the key using PBKDF2.
-    b_password = PBKDF2(password, b_salt, ALGORITHM_KEY_SIZE, PBKDF2_ITERATIONS)
+    key = PBKDF2(password=password, salt=b_salt, dkLen=ALGORITHM_KEY_SIZE, count=PBKDF2_ITERATIONS)
 
     # Create the cipher.
-    cipher = AES.new(b_password, mode=AES.MODE_GCM, nonce=b_nonce)
+    cipher = AES.new(key, mode=AES.MODE_GCM, nonce=b_nonce)
+    cipher.update(b_associatedText)
 
     # Decrypt
-    plaintext = cipher.decrypt(b_ciphertext)[:AES.block_size]
+    plaintext = cipher.decrypt(b_ciphertext)
 
     return plaintext
 
@@ -62,14 +65,20 @@ def main():
 
     (options, args) = parser.parse_args()
 
+
     decryptedText = decryptStr(options.encrypted_b64, options.magicStr)
 
     if len(decryptedText) > 0:
         print(colored("FOUND", 'green'))
-        print("decryptedText:" + str(decryptedText))
+        print("Password: {}".format(decryptedText.decode("utf-8")))
+
     else:
         print(colored("NOT FOUND", 'red'))
 
 if __name__ == '__main__':
     main()
 
+'''
+pip3 install pycryptodomex==3.8.1
+pip3 install termcolor
+'''
